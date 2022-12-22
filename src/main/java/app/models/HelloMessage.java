8049fc27f2eb851b5d01410ef4d1d7a43076908b
Juan_Huguet <jhuguetospina@gmail.com>
2022-12-22 09:24:55 -0500

    ADD: basic models

---
+++ src/main/java/app/models/HelloMessage.java
@@ -0,0 +1,18 @@
+package app.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+}
