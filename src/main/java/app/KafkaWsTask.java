ed2fd8788252ad01b9aff4a22fd84c826b4c562c
Juan_Huguet <jhuguetospina@gmail.com>
2022-12-22 09:25:09 -0500

    ADD: main class

---
+++ src/main/java/app/KafkaWsTask.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWsTask {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaWsTask.class, args);
+    }
+}
