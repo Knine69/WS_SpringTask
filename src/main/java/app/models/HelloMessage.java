f41b4d3c4b24fb726cda67543a99445d3b1f0576
Juan_Huguet <jhuguetospina@gmail.com>
2023-01-10 11:04:45 -0500

    REFACTOR: use of utility annotations

--- src/main/java/app/models/HelloMessage.java
+++ src/main/java/app/models/HelloMessage.java
@@ -1,18 +1,14 @@
 package app.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelloMessage {
-
     private String name;
-
-    public HelloMessage() {
-    }
-
-    public HelloMessage(String name) {
-        this.name = name;
-    }
 }
